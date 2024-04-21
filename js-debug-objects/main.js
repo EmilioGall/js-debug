47132b70e86bc35f84ff1dbe81ef4fb9062f1853
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-21 19:55:48 +0200

    add: solutions - debug-objects - js

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -84,3 +84,10 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+// 1- Questo codice definisce la costante "cars" di tipo array con elementi di tipo object. Definisce la costante "gasolineCars" di tipo array con gli elementi di "cars" che hanno type "benzina". Definisce la costante "dieselCars" di tipo array con gli elementi di "cars" che hanno type "diesel". Definisce la costante "otherCars" di tipo array con gli elementi di "cars" che hanno type diverso da "benzina" e "diesel". Stampa in console il messaggio "Auto a benzina". Stampa in console il messaggio "*******************************". Stampa in console gli elementi dell'array "gasolineCars". Stampa in console il messaggio "Auto a diesel". Stampa in console il messaggio "*******************************". Stampa in console gli elementi dell'array "dieselCars". Stampa in console il messaggio "Tutte le altre auto". Stampa in console gli elementi dell'array "otherCars".
+// 2- Errore sintassi: manca "," dopo "}" (riga 59).
+// -- Errore sintassi: arrow function è definita come "=>" non come ">=" (riga 67).
+// -- Errore sintassi: assenza di "return" (riga 71).
+// 3- Errore logica: andrebbe usato operatore logico "&&" anziché "||" (riga 74).
+// -- Errore logica: assenza del controllo sulle lettere maiuscole/minuscole (riga 67 - 70 - 74).
